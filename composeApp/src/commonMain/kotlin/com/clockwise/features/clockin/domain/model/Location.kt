fd54ffec129ba7f9af35ddaecc039331b401b1dc
Martin Hristev <mhristev03@gmail.com>
2025-07-27 19:45:01 +0300

    feat(di): Implement AndroidLocationServiceImpl in platform module

--- composeApp/src/commonMain/kotlin/com/clockwise/features/clockin/domain/model/Location.kt
+++
@@ -1,9 +0,0 @@
-package com.clockwise.features.clockin.domain.model
-
-/**
- * Data class representing a location with latitude and longitude
- */
-data class Location(
-    val latitude: Double,
-    val longitude: Double
-)
--- composeApp/src/commonMain/kotlin/com/clockwise/features/location/domain/model/LocationResult.kt
+++
@@ -1,9 +0,0 @@
-package com.clockwise.features.location.domain.model
-
-sealed class LocationResult {
-    data class Success(val location: Location) : LocationResult()
-    data class Error(val message: String) : LocationResult()
-    object Loading : LocationResult()
-    object PermissionDenied : LocationResult()
-    object LocationDisabled : LocationResult()
-}
